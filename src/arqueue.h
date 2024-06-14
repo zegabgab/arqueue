d052ff2d515c5836d06fad5316fc0d0d63dc0fe6
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-06-14 17:57:28 +0200

    Start of a macro definition to generify the queue

--- src/arqueue.h
+++ src/arqueue.h
@@ -17,3 +17,6 @@ bool dequeue(arqueue *queue, int *dest);
 bool queue_full(const arqueue *queue);
 
 bool queue_empty(const arqueue *queue);
+
+#define ARQUEUE(type, name)\
+typedef struct { type *data; size_t head; size_t length; size_t capacity; } name;\
